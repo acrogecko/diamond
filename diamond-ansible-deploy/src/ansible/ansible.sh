2bdf38ac2a73488d1f9ce6c9d200f53972d246c7
Bill Meine <wmeine@bill-meines-macbook-pro.local>
2017-07-27 02:02:08 -0600

    Initial repo load.

---
+++ diamond-ansible-deploy/src/ansible/ansible.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Run ansible to configure container host and deploy container
+# 
+# Note: the playbook would normally be run by some orchestration (e.g. Jenkins, Go, TeamCity)
+
+# Check ansible configuration
+#
+ansible dmd_daemons -i hosts --private-key=KeyPair.pem -u admin -m ping
+
+# Run the project playbook
+#
+ansible-playbook main.yaml -i ./hosts --private-key=KeyPair.pem -u admin --extra-vars "docker_tag=${project.version}"
